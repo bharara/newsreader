862fdd1619df50a2203e494e9bf729226d830d6e
Abdul Hadi <ahadi.bese17seecs@seecs.edu.pk>
2023-08-02 20:02:06 +0500

    current progress

---
+++ archiver.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from story import Story
+import logging
+from datetime import date
+import data_manager
+
+class Archiver:
+    stories = []
+
+    def __init__(self, date:date):
+        self.date = date.day
+        self.month = date.month
+        self.year = date.year
+        self.logger = logging.getLogger(__name__)
+
+    def url(self, page=1):
+        return f"https://www.wsj.com/news/archive/{self.year}/{self.month:02d}/{self.date:02d}?page={page}"
+    
+    def getStories(self, driver:webdriver):
+        self.logger.info(f"Getting stories for {self.year}/{self.month:02d}/{self.date:02d}")
+        page = 1
+        count = 50
+        while count > 0:
+            count = self.__getStoriesForPage__(driver, page)
+            page += 1
+        self.logger.info(f"Got {len(self.stories)} stories")
+        
+    def __getStoriesForPage__(self, driver:webdriver, page):
+        self.logger.info(f"Getting page {page}")
+        driver.get(self.url(page))
+        articles = driver.find_elements(By.CSS_SELECTOR, 'article')
+        self.logger.info(f"Found {len(articles)} articles")
+
+        for article in articles:
+            url = article.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+            category = article.find_element(By.CSS_SELECTOR, 'div.WSJTheme--articleType--34Gt-vdG').text
+            title = article.find_element(By.CSS_SELECTOR, 'span.WSJTheme--headlineText--He1ANr9C').text
+            img = article.find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
+            timestamp = article.find_element(By.CSS_SELECTOR, 'p').text
+            self.stories.append(Story(url, title, category, timestamp, img))
+
+        return len(articles)
+    
+    def toCSV(self):
+        self.logger.info(f"Saving stories to csv for {self.year}/{self.month:02d}/{self.date:02d}")
+        output_path = data_manager.saveStories(self.stories, self.year, self.month, self.date)
+        self.logger.info(f"Saved stories to csv at {output_path}/{self.date:02d}.csv")
---
+++ ui.py
@@ -0,0 +1,62 @@
+from datetime import date
+
+import streamlit as st
+
+import data_manager
+import utils
+from story import Story
+
+def logbox():
+    return st.info("Logs")
+
+def sideBar(lb):
+    if st.sidebar.button("Exit App"):
+        utils.exit()
+
+    st.sidebar.markdown("---")
+
+    st.sidebar.title("User Profile")
+    st.sidebar.write("Enter your details below:")
+
+    email, password, keywords = data_manager.getUserData()
+    new_email = st.sidebar.text_input("Email", email)
+    new_password = st.sidebar.text_input("Password", password, type="password")
+    new_keywords = st.sidebar.text_area("Keywords (comma-separated)", keywords)
+
+    if st.sidebar.button("Save"):
+        data_manager.saveUserData(new_email, new_password, new_keywords)
+        st.sidebar.success("Data saved successfully!")
+    
+
+def datePickerRow(lb):
+    col1, col2 = st.columns([4,1])
+    selected_date = col1.date_input("Select a date", max_value=date.today(), on_change=utils.dateChanged, key='selected_date')
+    col2.button("Fetch", on_click=utils.clickFetch, args=([lb]))
+    return selected_date
+
+
+def table(df, lb):
+    colms = st.columns((1,5,2,3,10))
+    fields = ["№", 'Headline', 'Category', 'Action', 'Summary']
+    for col, field_name in zip(colms, fields):
+        col.write(field_name)
+
+    for x, title in enumerate(df['title']):
+        c1, c2, c3, c4, c5 = st.columns((1,5,2,3,10))
+        c1.write(x)
+        c2.write(title)
+        c3.write(df['category'][x])
+        button_phold = c4.empty()
+        do_action = button_phold.button("Summarize", key=x)
+        c5.write("...")
+        if do_action:
+            lb.info(f"Fetching article {df['title'][x]}")
+            story = Story(df["url"][x], df["title"][x], df["category"][x], df["published"][x], df["image"][x])
+            try:
+                story.getStoryContent(st.session_state.driver, st.session_state.handler)
+                lb.info(f"Summarizing article with length {len(story.content)}")
+                story.getSummary()
+                c5.empty()
+                c5.write(story.summary)
+            except:
+                lb.error(f"Error getting article  {story.title}")
